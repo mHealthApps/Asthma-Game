d3c43cfdffa2b083800b856b1caf30c337d6c1fc
Adriel White <156387009+AdrielWhite@users.noreply.github.com>
2024-06-18 22:59:34 -1000

    002 setup and progress bar

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import ListPage from "./pages/ListPage";
+import DemoStacked from './pages/DemoStacked';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<></>} />
         <Route path="/list-page" element={<ListPage />} />
+        <Route path="/stacked-demo" element={<DemoStacked />} />
       </Routes>
     </Router>
   );
