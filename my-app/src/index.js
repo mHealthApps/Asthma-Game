c930a5ead2b1e3eebe6a8678bed15f1f08cf13ad
shehan <shehan.peruma@gmail.com>
2024-07-03 14:34:23 -1000

    Revert "base structure"
    This reverts commit d1b8cef9

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,19 @@
 import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom';
-// import ListPage from "./pages/ListPage";
+import ListPage from "./pages/ListPage";
+import StackedPageOne from './pages/StackedPageOne';
 
 
 function App() {
   return (
-      // <Router>
-      //   <Routes>
-      //     <Route exact path="/" element={<></>} />
-      //     <Route path="/list-page" element={<ListPage />} />
-      //   </Routes>
-      // </Router>
-      <p>Hello, World!</p>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<></>} />
+        <Route path="/list-page" element={<ListPage />} />
+        <Route path="/stacked-page-one" element={<StackedPageOne />} />
+      </Routes>
+    </Router>
   );
 }
 
