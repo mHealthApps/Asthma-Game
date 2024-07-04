7799fba5c84fab09399d08fffbccc5fb4dc51338
shehan <shehan.peruma@gmail.com>
2024-07-03 15:23:42 -1000

    more experiments for github pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import ListPage from "./pages/ListPage";
 
+
+function Home() {
+    return (
+        <div>
+            <p>Hello, World!</p>
+            <Link to="/list-page">Go to List Page</Link>
+        </div>
+    );
+}
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={<></>} />
+                <Route exact path="/" element={<Home />} />
                 <Route path="/list-page" element={<ListPage />} />
             </Routes>
         </Router>
