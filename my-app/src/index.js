caf16b57d6ed17b5194d6835ba3081a19ed941f3
shehan <shehan.peruma@gmail.com>
2024-07-03 15:13:13 -1000

    updated

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,19 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import ListPage from "./pages/ListPage";
-import StackedPageOne from './pages/StackedPageOne';
 
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<></>} />
-        <Route path="/list-page" element={<ListPage />} />
-        <Route path="/stacked-page-one" element={<StackedPageOne />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<></>} />
+                <Route path="/list-page" element={<ListPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
