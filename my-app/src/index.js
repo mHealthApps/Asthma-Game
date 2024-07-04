73720c11c16dab2bb000af147403e6861421a7d7
shehan <shehan.peruma@gmail.com>
2024-07-03 14:48:11 -1000

    trying out HashRouter since its supported by GitHub Pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import ListPage from "./pages/ListPage";
 import StackedPageOne from './pages/StackedPageOne';
