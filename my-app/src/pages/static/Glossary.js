cfc488f8b1782b215887040f233484c6c6f51eb2
Adriel White <156387009+AdrielWhite@users.noreply.github.com>
2024-10-26 16:32:24 -1000

    hard code send in glossary

--- my-app/src/pages/static/Glossary.js
+++ my-app/src/pages/static/Glossary.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../style.css';
 import TopBar from '../../components/TopBar';
 import useOrientation from '../../hooks/useOrientation';
 import useSendPageview from '../../hooks/useSendPageview';
+import ReactGA from 'react-ga4';
 
 
 const Glossary = () => {
   // GA Glossary pageview
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: '/glossary', title: 'Glossary Page' });
+  }, [])
   useSendPageview('Glossary Page');
 
   const orientation = useOrientation();
