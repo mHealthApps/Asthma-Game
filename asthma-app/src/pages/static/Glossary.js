f13913f1e69a8c4761f530a91012d5afa4a4b894
Adriel White <156387009+AdrielWhite@users.noreply.github.com>
2025-03-21 19:17:16 -1000

    new glossary content and styling

--- asthma-app/src/pages/static/Glossary.js
+++ asthma-app/src/pages/static/Glossary.js
@@ -21,10 +21,51 @@ const Glossary = () => {
         <TopBar oneLine='Glossary' orientation={orientation}/>
       </div>
       <div className='home-gray-container padding-5'>
-        <p style={{
+        <div className='resources-content' style={{
           fontSize: window.innerHeight * 0.022,
-        }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+        }}>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Windpipe</div> - the air passage from the throat, also known as the trachea
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Bronchus</div> - major air passage of the lung which diverge from the windpipe
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Bronchioles</div> - tiny branches into which the bronchus divides
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Alveoli</div> - tiny air sacs in the lungs which allows for gaseous exchange
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Lungs</div> -a major organ of the respiratory system, located in the rib cage and allows the exchange of oxygen and carbon dioxide through the bloodstream
+          </div>
+          <br/>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Rescue Inhaler</div> - quick relief medication; often used as needed or in emergency situations to relieve shortness of breath, wheezing, and chest tightness.
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Long-acting Inhaler</div> - used to control asthma symptoms.
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Combination Inhaler</div> - contains two or more active ingredients inhaled together to treat respiratory conditions such as asthma
+          </div>
+          <br/>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Hano</div> – asthma
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>‘Ohana</div> – family
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Keiki</div> – child
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>Uma</div> - chest
+          </div>
+          <div className='glossary-item'>
+            <div className='glossary-content-title'>VOG</div> – volcanic smog
+          </div>
+        </div>
       </div>
     </div>
   );
