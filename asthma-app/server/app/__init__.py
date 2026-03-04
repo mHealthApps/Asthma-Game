23b62859c20efbe4c1e74bd812eaa94541c3d1ff
GabrielWhite808 <156386680+GabrielWhite808@users.noreply.github.com>
2026-03-03 19:44:15 -1000

    Migrations

--- asthma-app/server/app/__init__.py
+++ asthma-app/server/app/__init__.py
@@ -1,13 +1,17 @@
+import os
 from flask import Flask
-from .extensions import db
-from config import Config
+from .extensions import db, migrate
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
+    from . import models
     from .routes import main
     app.register_blueprint(main)
 
--- asthma-app/server/app/extensions.py
+++ asthma-app/server/app/extensions.py
@@ -1,4 +1,6 @@
 # server/app/extensions.py
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
