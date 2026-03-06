9ab6a54bd5c856e0954931a84a3f257bd690f886
GabrielWhite808 <156386680+GabrielWhite808@users.noreply.github.com>
2026-03-05 22:41:32 -1000

    Updated User

--- asthma-app/server/app/models.py
+++ asthma-app/server/app/models.py
@@ -1,8 +1,18 @@
 from .extensions import db
 
 class User(db.Model):
+    #required information
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    firstname = db.Column(db.String(80))
+    lastname = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    #extra information
+    agerange = db.Column(db.String(50))
+    gender = db.Column(db.String(50))
+    demographic = db.Column(db.String(50))
+
 
     def __repr__(self):
         return f"<User {self.username}>"
