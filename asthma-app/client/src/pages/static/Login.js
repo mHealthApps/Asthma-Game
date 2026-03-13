e1483b938250163b197d903099c702ebc7d5fd26
GabrielWhite808 <156386680+GabrielWhite808@users.noreply.github.com>
2026-03-12 21:58:55 -1000

    Create Login.js

---
+++ asthma-app/client/src/pages/static/Login.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../style.css';
+import TopBar from '../../components/TopBar';
+
+const Login = () => {
+
+}
+
+export default Login;
