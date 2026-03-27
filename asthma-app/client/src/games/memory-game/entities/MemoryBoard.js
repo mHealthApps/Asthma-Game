7398ddf20376b88c731eb699c3a148c993092001
Adriel White <156387009+AdrielWhite@users.noreply.github.com>
2026-03-26 18:35:47 -1000

    memory click and flip

--- asthma-app/client/src/games/memory-game/entities/MemoryBoard.js
+++ asthma-app/client/src/games/memory-game/entities/MemoryBoard.js
@@ -1,19 +1,26 @@
 import { Container, Assets } from 'pixi.js';
 import MemoryCard from './MemoryCard';
 import Background_1 from '../../../assets/images/1_Background.jpg';
+import Blue_Inhaler_v2_21 from '../../../assets/images/21_Blue Inhaler_v2.jpg';
+
 
 export default class MemoryBoard {
     constructor(x, y, size) {
-        this.preload();
         this.view = new Container();
         this.view.x = x;
         this.view.y = y;
-        // this.view.anchor.set(0.5);
-        // this.view.scale(size / 500);
+        this.size = size;
+        this.activeCard = -1;
+        this.allowInteraction = true;
         this.cards = [];
 
+        this.generateCards();
+    }
+
+    async generateCards() {
+        await this.preload();
         for (let i = 0; i < 16; i++) {
-            const card = new MemoryCard(((i % 4) - 2) * 125, Math.floor((i / 4) - 2) * 125, 'card-top');
+            const card = new MemoryCard(((i % 4) - 1.5) * (this.size * 5 / 19), (Math.floor((i / 4)) -1.5) * (this.size * 5 / 19), i, 'blue-inhaler', (this.size * 4 / 19), this.handleCardClick.bind(this));
             this.cards.push(card);
             this.view.addChild(card.view);
         }
@@ -23,10 +30,36 @@ export default class MemoryBoard {
         // Create an array of asset data to load.
         this.assets = [
             { alias: 'card-top', src: Background_1 },
+            { alias: 'blue-inhaler', src: Blue_Inhaler_v2_21 },
         ];
+
+        this.imageSizes = []
 
         // Load the assets defined above.
         await Assets.load(this.assets);
+    }
+
+    handleCardClick = function(index) {
+        if (!this.allowInteraction || this.activeCard === index) {
+            return false;
+        }
+        if (this.activeCard > -1) {
+            // TODO: Check if pair
+            this.timeoutInteraction(index);
+        } else {
+            this.activeCard = index;
+        }
+        return true;
+    }
+
+    async timeoutInteraction(index) {
+        this.allowInteraction = false;
+        setTimeout(() => {
+            this.cards[index].resetFlip();
+            this.cards[this.activeCard].resetFlip();
+            this.allowInteraction = true;
+            this.activeCard = -1;
+        }, 1500);
     }
 
     update() {
--- asthma-app/client/src/games/memory-game/entities/MemoryCard.js
+++ asthma-app/client/src/games/memory-game/entities/MemoryCard.js
@@ -1,19 +1,46 @@
-import { Container, Sprite } from 'pixi.js';
+import { Container, Sprite, Texture,Graphics } from 'pixi.js';
 
 export default class MemoryCard {
-    constructor(x, y, type) {
+    constructor(x, y, index, type, width, onClick) {
         this.type = type;
 
         this.view = new Container();
         this.view.x = x;
         this.view.y = y;
+        this.width = width;
 
-        this.card = Sprite.from(this.type);
-        this.card.scale.set(0.067);
+        this.card = Sprite.from('card-top');
+        this.card.anchor.set(0.5);
+        this.card.scale.set(width * 0.01);
+
+        // Enable interaction
+        this.card.eventMode = 'static';
+        this.card.cursor = 'pointer';
+
+        this.card.on('pointerdown', () => {
+            if (onClick(index)) {
+                this.card.texture = Texture.from(this.type);
+                this.card.width = width;
+                this.card.height = width;
+            }
+        });
+
+        // Create rounded rectangle mask
+        this.mask = new Graphics()
+            .roundRect(-(width / 2), -(width / 2), width, width, width * 0.095)
+            .fill(0xffffff);
+        this.card.mask = this.mask;
+
+        this.view.addChild(this.mask);
         this.view.addChild(this.card);
     }
 
-    update() {
+    resetFlip() {
+        this.card.texture = Texture.from('card-top');
+        this.card.scale.set(this.width * 0.01);
+    }
 
+    update() {
+        
     }
 }
