12ada0c26afb4d029e51512f4dc131c6aec4b5a9
GabrielWhite808 <156386680+GabrielWhite808@users.noreply.github.com>
2026-03-27 13:11:03 -1000

    Added Login Route

--- asthma-app/server/app/__init__.py
+++ asthma-app/server/app/__init__.py
@@ -6,6 +6,10 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
 
+    app.config["JWT_SECRET_KEY"] = "temp-asthma432!" #temp hard-coded, change later
+
+    jwt = JWTManager(app)
+
     # allows the flask server to accept HTTP requests from localhost 3000
     CORS(app, origins=["http://localhost:3000"])
 
--- asthma-app/server/app/routes.py
+++ asthma-app/server/app/routes.py
@@ -1,5 +1,10 @@
 # server/app/routes.py
-from flask import Blueprint, jsonify
+import json
+from flask import Flask, request, Blueprint, jsonify
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
+
 
 main = Blueprint("main", __name__)
 
@@ -10,3 +15,19 @@ def health():
 @main.route('/api/test', methods=['GET'])
 def test():
     return {"message": "Flask is working!"}
+
+# Login Route
+@main.route('/api/token', methods=['POST'])
+def create_token():
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    # TEMP TEST LOGIN (replace later with DB)
+    if email != "test" or password != "test":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+
+    return {"access_token": access_token}
