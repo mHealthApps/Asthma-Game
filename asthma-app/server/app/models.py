68cb143ee3d2863c1986563490bf2e82131f35c3
GabrielWhite808 <156386680+GabrielWhite808@users.noreply.github.com>
2026-05-08 00:23:29 -1000

    Username field bug fixed

--- asthma-app/server/app/models.py
+++ asthma-app/server/app/models.py
@@ -16,7 +16,7 @@ class User(db.Model):
 
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return f"<User {self.email}>"
 
 #information on what modules the user has completed
 class Completion(db.Model):
